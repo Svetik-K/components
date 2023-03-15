55abc73fb9f41ee86a50de0bd714708e8d59b420
Svetik-K <sviatlana_kliautsova@icloud.com>
2023-03-15 19:35:12 +0100

    feat: add routing to project

--- task-components/src/App.tsx
+++ task-components/src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ task-components/src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  render() {
+    return <h1>This is Main page</h1>;
+  }
+}
+
+export default MainPage;
