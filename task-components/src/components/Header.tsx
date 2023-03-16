56670ae8a8e50592f5e8d5f3dfd55bace5218740
Svetik-K <sviatlana_kliautsova@icloud.com>
2023-03-16 11:27:44 +0100

    feat: add header class

--- task-components/src/components/Header.tsx
+++ task-components/src/components/Header.tsx
@@ -1,1 +1,26 @@
 import React from 'react';
+
+type HeaderProps = {
+  children?: React.ReactNode;
+};
+
+class Header extends React.Component<HeaderProps> {
+
+  render() {
+    const headerStyle = {
+			display: 'flex',
+      width: '100%',
+      padding: '20px',
+      backgroundColor: '#000000',
+      color: '#FFFFFF',
+    };
+
+    return (
+			<header style={headerStyle}>
+				{this.props.children}
+			</header>
+		)
+  }
+}
+
+export default Header;
--- task-components/src/pages/MainPage.tsx
+++ task-components/src/pages/MainPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class MainPage extends React.Component {
   render() {
-    return <h1>This is Main page</h1>;
+    return <Header><h1>Welcome!</h1></Header>;
   }
 }
 
