595be108987f6029d379d6cce2fa464f99abc38f
Svetik-K <sviatlana_kliautsova@icloud.com>
2023-03-19 09:34:23 +0100

    fix: linter issues

--- task-components/src/tests/App.test.tsx
+++ task-components/src/tests/App.test.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import { render, screen, fireEvent, getByTestId } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
-import userEvent from '@testing-library/user-event';
 
-describe("App", () => {
-  it("renders heading on Main page", () => {
+describe('App', () => {
+  it('renders heading on Main page', () => {
     render(<App />);
     expect(screen.getByText(/enhance/i)).toBeInTheDocument();
-  })
+  });
 
-  it("renders heading on About Us page", () => {
+  it('renders heading on About Us page', () => {
     render(<App />);
     expect(screen.getByText(/about us/i)).toBeInTheDocument();
-  })
+  });
 
-  it("two navigation links are shown on page", () => {
-		render(<App />);
-		expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
-		expect(screen.getByRole("link", { name: "About Us" })).toBeInTheDocument();
-	})
-
-  it("renders search field", () => {
+  it('two navigation links are shown on page', () => {
     render(<App />);
-    expect(screen.getByRole("textbox")).toBeInTheDocument();
-  })
+    expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'About Us' })).toBeInTheDocument();
+  });
 
+  it('renders search field', () => {
+    render(<App />);
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
 });
-
--- task-components/src/tests/SearchBar.test.tsx
+++ task-components/src/tests/SearchBar.test.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-describe("Search field", () => {
-  it("the value is entered in the search field", () => {
-    render(<SearchBar/>)
-    userEvent.type(screen.getByRole("textbox"), "snow");
-    expect(screen.getByRole("textbox")).toHaveDisplayValue("snow");
-  })
-})
+describe('Search field', () => {
+  it('the value is entered in the search field', () => {
+    render(<SearchBar />);
+    userEvent.type(screen.getByRole('textbox'), 'snow');
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('snow');
+  });
+});
