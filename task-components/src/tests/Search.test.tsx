48662850d64ca3f6e0bccc94ac1dfadb0159367a
Svetik-K <sviatlana_kliautsova@icloud.com>
2023-04-01 11:46:13 +0200

    add: test for saving to local storage

--- task-components/src/tests/Search.test.tsx
+++ task-components/src/tests/Search.test.tsx
@@ -2,6 +2,7 @@ import Search from 'components/Search';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import App from 'App';
 
 const filterCards = jest.fn();
 
@@ -12,11 +13,14 @@ describe('Search field', () => {
     expect(screen.getByRole('textbox')).toHaveDisplayValue('snow');
   });
 
-  // it('the entered value is saved to local storage', async () => {
-  //   localStorage.clear();
-  //   render(<Search filterCards={filterCards} />);
-  //   await userEvent.type(screen.getByRole('textbox'), 'tram');
-  //   const savedSearch: string = localStorage.savedValue;
-  //   expect(savedSearch).toEqual('tram');
-  // });
+  it('the entered search value is saved to local storage and displayed after unmount and mount again', async () => {
+    localStorage.clear();
+    render(<App />);
+    await userEvent.type(screen.getByRole('textbox'), 'tram');
+    await userEvent.click(screen.getByRole('link', { name: /form/i }));
+    await userEvent.click(screen.getByRole('link', { name: /home/i }));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('tram');
+    const savedSearch: string = localStorage.savedValue;
+    expect(savedSearch).toEqual('tram');
+  });
 });
