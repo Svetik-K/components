61f4e463399d5f5b7951e3a2e8819a9a0d555f91
Svetik-K <sviatlana_kliautsova@icloud.com>
2023-04-09 16:18:46 +0200

    fix: linter issues

--- task-components/src/tests/CardModal.test.tsx
+++ task-components/src/tests/CardModal.test.tsx
@@ -7,21 +7,21 @@ import userEvent from '@testing-library/user-event';
 const mockCard = {
   id: 2,
   name: 'Morty Smith',
-  image: '../mock/rick.png'  
-}
+  image: '../mock/rick.png',
+};
 
 describe('Api card', () => {
-	test('renders a modal when API call succeeds', async () => {
-		render(<ApiCard apiCard={mockCard} />);
-		expect(screen.getByText(/Morty Smith/i)).toBeInTheDocument();
-		await userEvent.click(screen.getByText(/Morty Smith/i));
-		await waitFor(() => {
-			expect(screen.getByText(/Personal file/i)).toBeInTheDocument();
-			expect(screen.getByText(/Alive/i)).toBeInTheDocument();
-			expect(screen.getByText(/Human/i)).toBeInTheDocument();
-			expect(screen.getByText(/Male/i)).toBeInTheDocument();
-			expect(screen.getByText(/unknown/i)).toBeInTheDocument();
-			expect(screen.getByText(/Citadel of Ricks/i)).toBeInTheDocument();
-		})
-	})
-})
+  test('renders a modal when API call succeeds', async () => {
+    render(<ApiCard apiCard={mockCard} />);
+    expect(screen.getByText(/Morty Smith/i)).toBeInTheDocument();
+    await userEvent.click(screen.getByText(/Morty Smith/i));
+    await waitFor(() => {
+      expect(screen.getByText(/Personal file/i)).toBeInTheDocument();
+      expect(screen.getByText(/Alive/i)).toBeInTheDocument();
+      expect(screen.getByText(/Human/i)).toBeInTheDocument();
+      expect(screen.getByText(/Male/i)).toBeInTheDocument();
+      expect(screen.getByText(/unknown/i)).toBeInTheDocument();
+      expect(screen.getByText(/Citadel of Ricks/i)).toBeInTheDocument();
+    });
+  });
+});
